5797e6821f27650a9b5e85fc0f53dc132ed482fc
piotr-napadlek <fisheye@gazeta.pl>
2016-09-07 00:53:58 +0200

    reworked launch.bat, added hsql version management

--- launch.bat
+++ launch.bat
@@ -1,4 +1,5 @@
 call mvn clean install
-call cd spring-template-webapp
-call run.bat
-call cd..
+REM change port below as you require!
+
+java -jar spring-template-webapp/target/dependency/webapp-runner.jar --port 9713 spring-template-webapp/target/*.war
+start http://localhost:9713/
--- spring-template-webapp/run.bat
+++
@@ -1,2 +0,0 @@
-REM change port below as you require!
-java -jar target/dependency/webapp-runner.jar --port 9002 target/*.war
